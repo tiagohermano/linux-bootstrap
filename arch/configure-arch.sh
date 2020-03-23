5a82dd6ac9678ff348d011b580e912fd73f08d06
Tiago Hermano <tiagofhermano@gmail.com>
2020-03-23 16:22:55 -0300

    fix keyboard setting

--- arch/configure-arch.sh
+++ arch/configure-arch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Syncing pacman database..."
-sudo pacman -Syu
+sudo pacman-mirrors -g
+sudo pacman -Syyu
 
 echo "Updating pacman keys"
 sudo pacman-key --init
@@ -8,9 +9,8 @@ sudo pacman-key --populate archlinux
 sudo pacman-key --populate manjaro
 sudo pacman-key --refresh-keys
 
-echo "Updating pacman mirrors..."
-sudo pacman-mirrors -g
-
 echo "Setting keyboard layout..."
-sudo localectl set-x11-keymap br abnt2
+setxkbmap -model abnt2 -layout br -variant ,abnt2
 
+echo "Installing Node.js and npm..."
+pacman -S nodejs npm
