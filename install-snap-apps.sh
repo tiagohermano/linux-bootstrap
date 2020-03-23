cd50568a6bddbbcfd8db0bfebe9fd9228f7e1b2f
Tiago Hermano <tiagofhermano@gmail.com>
2020-03-23 16:21:56 -0300

    delete snap verification using apt

--- install-snap-apps.sh
+++ install-snap-apps.sh
@@ -2,27 +2,16 @@
 
 # Fill the array below with your favorite Snapcraft apps
 snap_apps=(
-  "slack"
+# "slack --classic" // Not working on some systems yet
   "postman"
   "chromium"
-  "code --classic"
+# "code --classic" // Not working on some systems yet
   "gimp"
   "arduino"
   "telegram-desktop"
   "dbeaver-ce"
   "vlc"
 )
-
-echo "Enabling Snap Package manager..."
-sudo apt update
-sudo apt install snapd
-if [ $? -eq 0 ]; then
-  echo "Snap installed.";
-  else
-    echo "Snap installation failed.";
-    exit 1;
-  fi;
-
 
 for app in "${snap_apps[@]}"; do
   echo "Instaling $app..."
