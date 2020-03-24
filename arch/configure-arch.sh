e5a7138d7a5c8c768f7422a53fe43337c323328c
Tiago Hermano <tiagofhermano@gmail.com>
2020-03-24 08:50:03 -0300

    fix vscode watch for changes in large projects

--- arch/configure-arch.sh
+++ arch/configure-arch.sh
@@ -3,7 +3,7 @@ echo "Syncing pacman database..."
 sudo pacman-mirrors -g
 sudo pacman -Syyu
 
-echo "Updating pacman keys"
+echo "Updating pacman keys..."
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
 sudo pacman-key --populate manjaro
@@ -14,3 +14,6 @@ setxkbmap -model abnt2 -layout br -variant ,abnt2
 
 echo "Installing Node.js and npm..."
 pacman -S nodejs npm
+
+echo "Enabling Visual Studio Code to watch a large project for changes..."
+echo fs.inotify.max_user_watches=524288 | sudo tee /etc/sysctl.d/40-max-user-watches.conf && sudo sysctl --system
--- ubuntu/configure-ubuntu.sh
+++ ubuntu/configure-ubuntu.sh
@@ -1,3 +1,7 @@
 #/bin/bash
 sudo apt update
 sudo apt upgrade -y
+
+# Enable Visual Studio Code to watch a large project for changes
+sudo echo "fs.inotify.max_user_watches=524288" >> /etc/sysctl.conf
+sudo sysctl -p
