8474c042d73c54eeb0f11cc1741f8d6a95a34d21
Tiago Hermano <tiagofhermano@gmail.com>
2020-03-30 11:46:35 -0300

    arch: add spotify local files support

--- arch/configure-arch.sh
+++ arch/configure-arch.sh
@@ -14,3 +14,12 @@ setxkbmap -model abnt2 -layout br -variant ,abnt2
 
 echo "Enabling Visual Studio Code to watch a large project for changes..."
 echo fs.inotify.max_user_watches=524288 | sudo tee /etc/sysctl.d/50-max-user-watches.conf && sudo sysctl --system
+
+echo "Installing media libraries... [Supports local files play in Spotify]"
+git clone https://github.com/ramedeiros/spotify_libraries.git
+cd spotify_libraries
+sudo cp lib* /usr/lib/
+sudo ln -frs /usr/lib/libavutil.so.52.6.100 /usr/lib/libavutil.so.52
+sudo ln -frs /usr/lib/libavformat.so.54.36.100 /usr/lib/libavformat.so.54
+sudo ln -frs /usr/lib/libavcodec.so.54.71.100 /usr/lib/libavcodec.so.54
+sudo ldconfig
