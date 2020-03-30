74c9de2b9ce4304dfda2a0f30221b7d6067af3eb
Tiago Hermano <tiagofhermano@gmail.com>
2020-03-30 11:55:01 -0300

    ubuntu: add spotify local files support

--- ubuntu/configure-ubuntu.sh
+++ ubuntu/configure-ubuntu.sh
@@ -5,3 +5,13 @@ sudo apt upgrade -y
 # Enable Visual Studio Code to watch a large project for changes
 sudo echo "fs.inotify.max_user_watches=524288" >> /etc/sysctl.conf
 sudo sysctl -p
+
+# Install aditional spotify media libraries
+echo "Installing spotify aditional media libraries..."
+git clone https://github.com/ramedeiros/spotify_libraries.git
+cd spotify_libraries
+sudo cp lib* /usr/lib/x86_64-linux-gnu/
+sudo ln -frs /usr/lib/x86_64-linux-gnu/libavutil.so.52.6.100 /usr/lib/x86_64-linux-gnu/libavutil.so.52
+sudo ln -frs /usr/lib/x86_64-linux-gnu/libavformat.so.54.36.100 /usr/lib/x86_64-linux-gnu/libavformat.so.54
+sudo ln -frs /usr/lib/x86_64-linux-gnu/libavcodec.so.54.71.100 /usr/lib/x86_64-linux-gnu/libavcodec.so.54
+sudo ldconfig
--- ubuntu/install-apt-packages.sh
+++ ubuntu/install-apt-packages.sh
@@ -1,9 +1,19 @@
 #/bin/bash
+echo "Installing Snap package manager..."
 sudo apt install snapd
 if [ $? -eq 0 ]; then
   echo "Snap installation complete.";
   else
     echo "Snap installation failed.";
+    exit 1;
+fi;
+
+echo "Installing media libraries..."
+sudo apt install ubuntu-restricted-extras ffmpeg libavcodec-extra libavcodec-extra57 libavutil55 libavformat57 zenity -y
+if [ $? -eq 0 ]; then
+  echo "Media libraries installation complete.";
+  else
+    echo "Media libraries installation failed.";
     exit 1;
 fi;
 
