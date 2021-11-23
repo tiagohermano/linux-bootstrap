fb4d6249d98b2e39e36de5efc268148c26656cd0
Tiago Hermano <tiagofhermano@gmail.com>
2021-11-23 09:57:21 -0300

    chore: update lunarvim's config

--- dotfiles/config.lua
+++ dotfiles/config.lua
@@ -1,23 +1,15 @@
---[[
-lvim is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
 -- general
-lvim.log.level = "warn"
-lvim.format_on_save = false
+lvim.format_on_save = true
 lvim.lint_on_save = true
 lvim.colorscheme = "tokyonight"
+lvim.relativenumber = true
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
--- lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- unmap a default keymapping
 -- lvim.keys.normal_mode["<C-Up>"] = ""
 -- edit a default keymapping
@@ -30,8 +22,21 @@ lvim.keys.normal_mode["<C-r>"] = ":lua vim.lsp.buf.rename()<CR>"
 lvim.keys.normal_mode["<leader>z"] = ":vs<CR>"
 lvim.keys.normal_mode["<leader>t"] = ":Telescope find_files<CR>"
 
+-- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
+-- lvim.builtin.telescope.on_config_done = function()
+--   local actions = require "telescope.actions"
+--   -- for input mode
+--   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
+--   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
+--   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
+--   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
+--   -- for normal mode
+--   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
+--   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
+-- end
+
 -- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>lua require'telescope'.extensions.project.project{}<CR>", "Projects" }
+-- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 -- lvim.builtin.which_key.mappings["t"] = {
 --   name = "+Trouble",
 --   r = { "<cmd>Trouble lsp_references<cr>", "References" },
@@ -46,19 +51,23 @@ lvim.keys.normal_mode["<leader>t"] = ":Telescope find_files<CR>"
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 1
-lvim.builtin.cmdheight = 0
-lvim.relativenumber = true
-lvim.cursorline = false
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 0
+vim.g.tokyonight_transparent_sidebar = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = "maintained"
 lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.autopairs.active = true
 lvim.transparent_window = true
-vim.g.tokyonight_transparent_sidebar = true
+lvim.format_on_save = false
 
 vim.g.tidal_target = "terminal"
+
+vim.opt.cmdheight = 1
+vim.opt.guifont = "monospace:16"
+vim.opt.relativenumber = true
+vim.opt.cursorline = false
 
 -- generic LSP settings
 -- you can set a custom on_attach function that will be used for all the language servers
@@ -69,6 +78,21 @@ vim.g.tidal_target = "terminal"
 --   end
 --   --Enable completion triggered by <c-x><c-o>
 --   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+-- you can overwrite the null_ls setup table (useful for setting the root_dir function)
+-- lvim.lsp.null_ls.setup = {
+--   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
+-- }
+-- or if you need something more advanced
+-- lvim.lsp.null_ls.setup.root_dir = function(fname)
+--   if vim.bo.filetype == "javascript" then
+--     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
+--       or require("lspconfig/util").path.dirname(fname)
+--   elseif vim.bo.filetype == "php" then
+--     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
+--   else
+--     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
+--   end
 -- end
 
 -- set a formatter if you want to override the default lsp one (if it exists)
@@ -93,15 +117,12 @@ lvim.plugins = {
         config = function() require"lsp_signature".on_attach() end,
         event = "InsertEnter"
     },
+    {'bluz71/vim-nightfly-guicolors'},
+    {'pineapplegiant/spaceduck'},
+    {'joshdick/onedark.vim'},
     {"tpope/vim-fugitive"},
-    {"bluz71/vim-moonfly-colors"},
-    {"bluz71/vim-nightfly-guicolors"},
-    {"haishanh/night-owl.vim"},
-    {"navarasu/onedark.nvim"},
-    {"yashguptaz/calvera-dark.nvim"},
-    {"shaunsingh/moonlight.nvim"},
     {"tidalcycles/vim-tidal"},
-    {"jiangmiao/auto-pairs"},
+    {"terryma/vim-multiple-cursors"}
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
